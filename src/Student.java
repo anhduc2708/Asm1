7b4e6c55f10d24109f9e9219214a2d631913c1a7
anhduc2708 <68212055+anhduc2708@users.noreply.github.com>
2022-03-15 17:03:39 +0700

    First commit

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private String ID;
+    private String name;
+    private String DoB;
+
+    public Student(String ID, String name, String doB) {
+        this.ID = ID;
+        this.name = name;
+        DoB = doB;
+    }
+}
